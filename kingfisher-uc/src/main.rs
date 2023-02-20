202825c58ede0245515e1b956705de05d1b03992
Ryan Wicks <ryancwicks@gmail.com>
2023-02-19 20:09:05 -0500

    Controller board compiling, no real code yet.

--- kingfisher-uc/src/main.rs
+++ kingfisher-uc/src/main.rs
@@ -1,27 +1,58 @@
 #![no_std]
 #![no_main]
+#![feature(abi_avr_interrupt)]
 
+use atmega32u4_usb_serial::UsbSerial;
+use core::sync::atomic::{AtomicBool, Ordering};
 use panic_halt as _;
+
+static PIN_CHANGED: AtomicBool = AtomicBool::new(false);
+
+//This function is called on change of pin 2
+#[avr_device::interrupt(atmega32u4)]
+#[allow(non_snake_case)]
+fn INT0() {
+    PIN_CHANGED.store(true, Ordering::SeqCst);
+}
+
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let mut usb = UsbSerial::new(dp.USB_DEVICE);
 
-    let mut led = pins.d13.into_output();
+    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
+
+    // This is wrong, need to look into the avr datasheet
+    // Enable the PCINT2 pin change interrupt
+    dp.EXINT.pcicr.write(|w| unsafe { w.bits(0b100) });
+
+    // Enable pin change interrupts on PCINT18 which is pin PD2 (= d2)
+    dp.EXINT.eimsk.write(|w| w.bits(0b100));
+
+    //digital outputs / control lights
+    let mut port_nav_light = pins.d11.into_output();
+    let mut _stb_nav_lights = pins.d3.into_output();
+    
+    //PWM outputs
+    let mut _std_ctrl = pins.d5.into_output();
+    let mut _port_ctl = pins.d13.into_output();
+    let mut _fan = pins.led_tx.into_output();
+
+    unsafe { avr_device::interrupt::enable() };
+
+    //analog/pwm inputs
+    let _throttle = pins.a0.into_analog_input(&mut adc);
+    //let direction = pins.d0.into_analog_input(&mut adc);
+    //let switch = pins.d1.into_analog_input(&mut adc);
+
+    usb.init(&dp.PLL);
 
     loop {
-        led.toggle();
+        port_nav_light.toggle();
         arduino_hal::delay_ms(1000);
+        ufmt::uwriteln!(usb, "Hello, World!").unwrap();
     }
 }
